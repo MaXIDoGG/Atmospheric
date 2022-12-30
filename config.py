68e21ca8e5db474035dc9d33d77047eca6de6f38
maksim nasonov <maks.nasonov.03@mail.ru>
2022-12-30 18:10:24 +0700

    delete config))))

--- config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'token': 'MTA0MTY5NTQ0NDE0MjY1NzU0Ng.GJu5ga.WWVvF1s8FdA1AWcYArSNMabWNNH73I-mAgviXY',
-    'bot': 'Atmospheric',
-    'id': 1041695444142657546,
-    'prefix': 'a!'
-}
